33a47a16e1430a162d38e5f24da9cc447e39a47f
tony48 <antolinares01@gmail.com>
2019-06-16 18:52:29 -0400

    dll and inclination cfg

---
+++ src/KSRSS/Tiles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace KSRSS
+{
+    [KSPAddon(KSPAddon.Startup.FlightAndKSC, false)]
+    public class TilesFix : MonoBehaviour
+    {
+        protected bool isSuborbital = false;
+
+        public void Start()
+        {
+            GameEvents.onVesselSituationChange.Add(OnSituationChanged);
+        }
+
+        public void OnDestroy()
+        {
+            GameEvents.onVesselSituationChange.Remove(OnSituationChanged);
+        }
+
+        private void OnSituationChanged(GameEvents.HostedFromToAction<Vessel, Vessel.Situations> data)
+        {
+            Vessel curVessel = data.host;
+            if (!curVessel.mainBody.isHomeWorld || !curVessel.isActiveVessel) return;
+
+            if (data.from == Vessel.Situations.FLYING && data.to == Vessel.Situations.SUB_ORBITAL)
+            {
+                isSuborbital = true;
+            }
+            else if (isSuborbital && data.to == Vessel.Situations.FLYING)
+            {
+                isSuborbital = false;
+                Debug.Log("[KSRSS]: Calling StartUpSphere() to prevent missing PQ tiles");
+                curVessel.mainBody.pqsController.StartUpSphere();
+            }
+        }
+    }
+}
---
+++ src/KSRSS/rick.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace KSRSS
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    public class Rick : MonoBehaviour
+    {
+        internal void Start()
+        {
+            if ((DateTime.Now.Day == 1 && DateTime.Now.Month == 4) || Environment.GetCommandLineArgs().Contains("-nggyu"))
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
+
+}
